a441d8055d9bbedb24dfd07c9071398564f6b186
AngeloDG <gamedreymon2@gmail.com>
2025-12-16 19:17:03 +0100

    file per flushare tutti i nodi

---
+++ flush_arp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NODI="r1 r2 r3 r4 r5 r6 r7 r8 r9 r10 r11 veneto liguria toscana marche campania puglia calabria sardegna sicilia"
+
+echo "--- Pulizia tabelle ARP su tutti i nodi ---"
+
+
+for NODO in $NODI; do
+    if kathara exec "$NODO" "ip neigh flush all"; then
+        echo "[OK] $NODO: ARP pulita."
+    else
+        echo "[ERRORE] $NODO: Impossibile pulire ARP (nodo non attivo o nome errato)."
+    fi
+done
+
+echo "--- Finito ---"
