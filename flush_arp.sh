9721215ae002c61faa9c1247ed7881e99f4421c3
AngeloDG <gamedreymon2@gmail.com>
2025-12-16 19:41:50 +0100

    commit

--- flush_arp.sh
+++ flush_arp.sh
@@ -2,15 +2,15 @@
 
 NODI="r1 r2 r3 r4 r5 r6 r7 r8 r9 r10 r11 veneto liguria toscana marche campania puglia calabria sardegna sicilia"
 
-echo "--- Pulizia tabelle ARP su tutti i nodi ---"
+echo "Pulizia tabelle ARP su tutti i nodi"
 
 
 for NODO in $NODI; do
     if kathara exec "$NODO" "ip neigh flush all"; then
-        echo "[OK] $NODO: ARP pulita."
+        echo "[OK] $NODO: ARP pulita"
     else
-        echo "[ERRORE] $NODO: Impossibile pulire ARP (nodo non attivo o nome errato)."
+        echo "[ERRORE] $NODO: Impossibile pulire ARP"
     fi
 done
 
-echo "--- Finito ---"
+echo "Finito"
